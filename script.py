278a66a5e2bb75ad6873ef110b7627dd3b475f48
Alan Vazquez <vazquala000@warren.k12.in.us>
2024-08-23 10:05:13 -0400

    Initial Commit

---
+++ script.py
@@ -0,0 +1,9 @@
+miles = 3
+minutes = 60
+hours = minutes / 60
+
+mph = miles / hours
+
+print("How many miles did you run?", miles)
+print("How many minutes did it take you?", minutes)
+print("Speed in mph:", mph)
